02d6acec9af576ee0d9175c11cb43b9e5285aee6
Hoàng An F8 <hoangan@fullstack.edu.vn>
2024-03-09 17:32:33 +0700

    Update title

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,42 +1,42 @@
-import React, { useEffect, useState } from 'react';
-import getBlogs from '../helpers/getBlogs';
-import { Link, NavLink, useSearchParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import getBlogs from "../helpers/getBlogs";
+import { Link, NavLink, useSearchParams } from "react-router-dom";
 
 export default function Home() {
-	const [searchParams] = useSearchParams();
-	const [blogs, setBlogs] = useState({
-		data: [],
-		max_page: 0,
-	});
-	const page = searchParams.get('page') || 1;
-	useEffect(() => {
-		getBlogs({}, page).then((data) => setBlogs(data));
-		window.scrollTo(0, 0);
-	}, [searchParams]);
-	if (!blogs.data.length) return <span>Loading...</span>;
-	return (
-		<div className='App'>
-			<header className='App-header'>
-				<h1 className='App-title'>Blogs</h1>
-			</header>
-			<ul>
-				{blogs.data.map((blog) => (
-					<li key={blog.blog_id} className='mb-2'>
-						<Link to={`/${blog.blog_id}`}>
-							{blog.title}
-							<img src={blog.image_url} width={400} />
-						</Link>
-					</li>
-				))}
-			</ul>
+  const [searchParams] = useSearchParams();
+  const [blogs, setBlogs] = useState({
+    data: [],
+    max_page: 0,
+  });
+  const page = searchParams.get("page") || 1;
+  useEffect(() => {
+    getBlogs({}, page).then((data) => setBlogs(data));
+    window.scrollTo(0, 0);
+  }, [searchParams]);
+  if (!blogs.data.length) return <span>Loading...</span>;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Blogs - Unicode</h1>
+      </header>
+      <ul>
+        {blogs.data.map((blog) => (
+          <li key={blog.blog_id} className="mb-2">
+            <Link to={`/${blog.blog_id}`}>
+              {blog.title}
+              <img src={blog.image_url} width={400} />
+            </Link>
+          </li>
+        ))}
+      </ul>
 
-			<div className='flex pb-4'>
-				{Array.from({ length: blogs.max_page }, (_, i) => (
-					<NavLink to={`?page=${i + 1}`} className={`btn`} key={i}>
-						{i + 1}
-					</NavLink>
-				))}
-			</div>
-		</div>
-	);
+      <div className="flex pb-4">
+        {Array.from({ length: blogs.max_page }, (_, i) => (
+          <NavLink to={`?page=${i + 1}`} className={`btn`} key={i}>
+            {i + 1}
+          </NavLink>
+        ))}
+      </div>
+    </div>
+  );
 }
